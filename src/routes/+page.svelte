01b4989ddb74501400b462aecc4fe2fd50b2356d
Joshua Kellman <joshua@twoals.com>
2024-07-23 09:15:17 -0400

    added Introduction element

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Introduction from "./Introduction.svelte";
+</script>
+
+<html lang="eng">
+    <head></head>
+    <style></style>
+    <body>
+        <Introduction></Introduction>
+        <div class="flex items-start justify-center min-h-screen">
+            <div class="w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12 p-4 mt-4"></div>
+        </div>
+    </body>
+</html>
---
+++ src/routes/Introduction.svelte
@@ -0,0 +1,7 @@
+<div class="bg-gradient-to-r from-blue-400 to-indigo-500 rounded-lg m-5 w-1/3">
+    <h1 class="p-1 pt-6 pl-5 font-semibold text-xl">Hi my name is</h1>
+    <h1 class="p-1 pl-32 pt-15 font-bold text-5xl font-sans">ThatOddOne</h1>
+    <h1 class="p-5 pt-3 pl-64 font-semibold text-xl">
+        I design apps and stuff
+    </h1>
+</div>
