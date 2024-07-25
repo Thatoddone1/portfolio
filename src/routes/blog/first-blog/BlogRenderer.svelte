8a7604bc600b57d44a80fbdfd92f50662914a723
Joshua Kellman <joshua@twoals.com>
2024-07-24 21:39:01 -0400

    added blogging system. still need to build page to access this, and navbar

---
+++ src/routes/blog/first-blog/BlogRenderer.svelte
@@ -0,0 +1,16 @@
+<script>
+    import MarkdownRenderer from "../MarkdownRenderer.svelte";
+    export let filename;
+    export let title;
+</script>
+
+<head>
+    <title>{title}</title>
+</head>
+<div class="flex flex-row flex-auto justify-center">
+    <div class="bg-slate-100 w-5/6 rounded-lg p-10 m-5">
+        <h1 class="text-3xl font-bold text-center">{title}</h1>
+        <hr />
+        <MarkdownRenderer {filename}></MarkdownRenderer>
+    </div>
+</div>
