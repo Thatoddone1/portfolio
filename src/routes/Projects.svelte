67e3169c72fc626f1c6f3792f57e3876cf373c19
JoshuaKellman <joshua@twoals.com>
2024-11-02 14:22:17 -0400

    removed unnecisary tracking of files

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -1,5 +1,6 @@
 <!---SimpleScript-->
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
+    <!--SimpleScript Div ↓-->
     <div
         class="rounded-lg bg-gradient-to-r from-blue-200 to-violet-200 dark:to-blue-700 dark:from-purple-700 p-10 pb-7 pt-7 transition-all duration-800 ease-in-out m-5 justify-center item-center"
     >
@@ -46,29 +47,6 @@
             <div class="p-5 pt-10 text-xl font-bold">Coming Soon!</div>
         </div>
     </div>
-    <!--Plan-it div
-    <div
-        class="rounded-lg bg-gradient-to-r from-rose-200 to-indigo-200 dark:to-rose-700 dark:from-indigo-700 p-10 pb-7 pt-7 transition-all duration-800 ease-in-out m-5 justify-center item-center"
-    >
-        <h1
-            class="text-center font-bold text-4xl p-1 text-blue-800 dark:text-blue-300"
-        >
-            Plan-it
-        </h1>
-        <p
-            class="font-medium text-xl text-center text-gray-500 dark:text-gray-300"
-        >
-            Students around the planet try to manage all of their school tasks,
-            to no avail. Presenting Plan-it <small>(see what I did there)</small
-            >, the online student planner of the future. Plan out your homework,
-            classes, syllabus, and grades, plus other non-school work in one
-            cohesive UI.
-        </p>
-        <div class="flex justify-center">
-            <div class="p-5 pt-10 text-xl font-bold">Coming Soon!</div>
-        </div>
-    </div>
-    -->
     <!--Quill ↓     -->
     <div
         class="rounded-lg bg-gradient-to-r from-red-200 to-teal-200 dark:to-red-700 dark:from-teal-700 p-10 pb-7 pt-7 transition-all duration-800 ease-in-out m-5 justify-center item-center"
@@ -88,6 +66,7 @@
             <div class="p-5 pt-10 text-xl font-bold">Coming Soon!</div>
         </div>
     </div>
+    <!--Potato Div ↓-->
     <div
         class="rounded-lg bg-gradient-to-r from-red-200 to-teal-200 dark:to-red-700 dark:from-teal-700 p-10 pb-7 pt-7 transition-all duration-800 ease-in-out m-5 justify-center item-center"
     >
@@ -105,27 +84,6 @@
             <a
                 class="p-3 m-5 bg-violet-500 rounded-md hover:shadow-lg hover:bg-violet-600 font-semibold dark:bg-violet-300 dark:hover:bg-violet-400"
                 href="https://potato.joshuarocks.me">Click some potatoes</a
-            >
-        </div>
-    </div>
-    <div
-        class="rounded-lg bg-gradient-to-r from-green-200 to-violet-200 dark:to-green-700 dark:from-violet-700 p-10 pb-7 pt-7 transition-all duration-800 ease-in-out m-5 justify-center item-center"
-    >
-        <h1
-            class="text-center font-bold text-4xl p-1 text-blue-800 dark:text-blue-300"
-        >
-            ZenDev Labs
-        </h1>
-        <p
-            class="font-medium text-xl text-center text-gray-500 dark:text-gray-300"
-        >
-            Check back here soon to see what we are making at zendev labs!
-        </p>
-        <div class="flex justify-center">
-            <a
-                class="p-3 m-5 bg-violet-500 rounded-md hover:shadow-lg hover:bg-violet-600 font-semibold dark:bg-violet-300 dark:hover:bg-violet-400"
-                href="https://github.com/ZenDev-Labs"
-                >Learn more about ZenDev Labs</a
             >
         </div>
     </div>
