01f0a2edf454c70108a285f49686b3a6b787f267
JoshuaKellman <joshua@twoals.com>
2024-11-05 10:10:11 -0500

    update BlogRenderer.svelte to enhance styling for better readability in light and dark modes

--- src/routes/blog/BlogRenderer.svelte
+++ src/routes/blog/BlogRenderer.svelte
@@ -8,8 +8,8 @@
     <title>{title}</title>
 </head>
 <div class="flex flex-row flex-auto justify-center">
-    <div class="bg-slate-100 w-5/6 rounded-lg p-10 m-5">
-        <h1 class="text-3xl font-bold text-center">{title}</h1>
+    <div class="bg-slate-100 dark:bg-slate-800 w-5/6 rounded-lg p-10 m-5">
+        <h1 class="text-3xl font-bold text-center text-black dark:text-white">{title}</h1>
         <hr />
         <MarkdownRenderer {filename}></MarkdownRenderer>
     </div>
